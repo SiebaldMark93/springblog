402d0c35e9786ed0ca80f7daf0ed95793adfed1b
Mark Siebald <siebald.mark93@gmail.com>
2022-07-26 15:56:45 -0500

    feat:controller exercise

---
+++ src/main/java/com/example/codeup/springblog/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello () {
+        return "Hello from Spring!";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "this is a test";
+    }
+}
